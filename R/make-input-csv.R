1f4697b6042c7551434961c7629d715319609abe
Dries Debeer <debeer.dries@gmail.com>
2025-05-05 16:22:21 +0200

    no quotes in csv

--- R/make-input-csv.R
+++ R/make-input-csv.R
@@ -4,4 +4,4 @@ input <- gsub("data_", "", list.files("data/data_raw"), fixed = TRUE)
 input <- gsub(".csv", "", input, fixed = TRUE)
 
 input <- data.frame(dataset = input)
-write.csv(input, "input/input.csv", row.names = FALSE)
+write.csv(input, "input/input.csv", row.names = FALSE, quote = FALSE)
