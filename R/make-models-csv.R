fca563b6ac0efa9e97f3ca9d131c3976466beaad
Dries Debeer <debeer.dries@gmail.com>
2025-05-07 18:27:32 +0200

    formulas in one line

--- R/make-models-csv.R
+++ R/make-models-csv.R
@@ -1,17 +1,9 @@
 models <- data.frame(name = c("ri", "rs1", "rs2", "rs3"),
                      formula = c(
-                       "m_f * (neg_aff_pm + neg_aff_pmc_lag) +
-                         (1 | ppID) +
-                         (1 | study)",
-                       "m_f * (neg_aff_pm + neg_aff_pmc_lag) +
-                         (1 + neg_aff_pm + neg_aff_pmc_lag | ppID) +
-                         (1 | study",
-                       "m_f * (neg_aff_pm + neg_aff_pmc_lag) +
-                         (1 + neg_aff_pm + neg_aff_pmc_lag | ppID) +
-                         (1 + neg_aff_pm + neg_aff_pmc_lag | study)",
-                       "m_f * (neg_aff_pm + neg_aff_pmc_lag) +
-                         (1 + neg_aff_pm + neg_aff_pmc_lag | ppID) +
-                         (1 + m_f + neg_aff_pm + neg_aff_pmc_lag | study)"
+                       "m_f * (neg_aff_pm + neg_aff_pmc_lag) + (1 | ppID) + (1 | study)",
+                       "m_f * (neg_aff_pm + neg_aff_pmc_lag) + (1 + neg_aff_pm + neg_aff_pmc_lag | ppID) + (1 | study",
+                       "m_f * (neg_aff_pm + neg_aff_pmc_lag) + (1 + neg_aff_pm + neg_aff_pmc_lag | ppID) + (1 + neg_aff_pm + neg_aff_pmc_lag | study)",
+                       "m_f * (neg_aff_pm + neg_aff_pmc_lag) + (1 + neg_aff_pm + neg_aff_pmc_lag | ppID) + (1 + m_f + neg_aff_pm + neg_aff_pmc_lag | study)"
                      ))
 
 write.csv(models, "input/models.csv", row.names = FALSE, quote = FALSE)
