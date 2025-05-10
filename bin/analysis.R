f91f3603dd2e81073e97c20dff509c23da2a24cf
Dries Debeer <debeer.dries@gmail.com>
2025-05-10 22:44:45 +0200

    no rescue?

--- bin/analysis.R
+++ bin/analysis.R
@@ -3,18 +3,13 @@
 # collect four trailing arguments: data_path, name, formula, threshold
 args <- commandArgs(trailingOnly=TRUE)
 data_path <- args[1]
-print(data_path)
 name <- args[2]
-print(name)
 formula <- args[3]
-print(formula)
 threshold <- args[4]
-print(threshold)
 
 
 # read the data
 data <- readRDS(data_path)
-dim(data)
 
 
 # create binary outcome
@@ -24,11 +19,11 @@ data$er_bin[data$type == "cont"] <- 1 * (data$er[data$type == "cont"] > threshol
 
 
 # rescue solution?
-formula <- switch(name,
-       "ri" = 'm_f*(neg_aff_pm+neg_aff_pmc_lag)+(1|ppID)+(1|study)',
-       "rs1" = 'm_f*(neg_aff_pm+neg_aff_pmc_lag)+(1+neg_aff_pm+neg_aff_pmc_lag|ppID)+(1|study)',
-       "rs2" = 'm_f*(neg_aff_pm+neg_aff_pmc_lag)+(1+neg_aff_pm+neg_aff_pmc_lag|ppID)+(1+neg_aff_pm+neg_aff_pmc_lag|study)',
-       "rs3" = 'm_f*(neg_aff_pm+neg_aff_pmc_lag)+(1+neg_aff_pm+neg_aff_pmc_lag|ppID)+(1+m_f+neg_aff_pm+neg_aff_pmc_lag|study)')
+# formula <- switch(name,
+#        "ri"  = 'm_f*(neg_aff_pm+neg_aff_pmc_lag)+(1|ppID)+(1|study)',
+#        "rs1" = 'm_f*(neg_aff_pm+neg_aff_pmc_lag)+(1+neg_aff_pm+neg_aff_pmc_lag|ppID)+(1|study)',
+#        "rs2" = 'm_f*(neg_aff_pm+neg_aff_pmc_lag)+(1+neg_aff_pm+neg_aff_pmc_lag|ppID)+(1+neg_aff_pm+neg_aff_pmc_lag|study)',
+#        "rs3" = 'm_f*(neg_aff_pm+neg_aff_pmc_lag)+(1+neg_aff_pm+neg_aff_pmc_lag|ppID)+(1+m_f+neg_aff_pm+neg_aff_pmc_lag|study)')
 
 
 # create formula
