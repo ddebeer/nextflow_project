27e77593b4dd9c9b11ff39961a0be5cb313389de
Dries Debeer <debeer.dries@gmail.com>
2025-05-10 22:28:00 +0200

    rescue solution 2

--- bin/analysis.R
+++ bin/analysis.R
@@ -3,13 +3,18 @@
 # collect four trailing arguments: data_path, name, formula, threshold
 args <- commandArgs(trailingOnly=TRUE)
 data_path <- args[1]
+print(data_path)
 name <- args[2]
+print(name)
 formula <- args[3]
+print(formula)
 threshold <- args[4]
+print(threshold)
 
 
 # read the data
 data <- readRDS(data_path)
+dim(data)
 
 
 # create binary outcome
